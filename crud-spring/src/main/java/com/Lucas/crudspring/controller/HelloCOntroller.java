bee0fafddc365e9faa8258e10a4f1d44c7bd0c19
lucasriss0 <lucasrisso13@gmail.com>
2026-05-12 10:10:27 -0300

    Projeto completo angular + spring

---
+++ crud-spring/src/main/java/com/Lucas/crudspring/controller/HelloCOntroller.java
@@ -0,0 +1,19 @@
+package com.Lucas.crudspring.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+
+
+
+
+public class HelloCOntroller {
+    @GetMapping
+    public  String hello(){
+        return "hello";
+    }
+}
